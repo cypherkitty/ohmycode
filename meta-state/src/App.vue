b4c25774f86d1c9d3a78ad5b0b62e17da48aa8e9
metabynull <bynull@meta-secret.org>
2023-11-11 00:20:10 -0800

    Meta State: initial commit

---
+++ meta-state/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <MetaState/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import MetaState from './components/MetaState.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    MetaState
+  }
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
---
+++ meta-state/src/components/MetaState.vue
@@ -0,0 +1,84 @@
+<script lang="ts">
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<template>
+  <div class="container">
+    <div class="card">
+      <h2 class="header">Empty State</h2>
+      <ul class="record-list">
+        <li class="record">text1</li>
+        <li class="record">text2</li>
+        <li class="record">text3</li>
+      </ul>
+    </div>
+
+    <div style="padding: 10px"></div>
+
+    <div class="card">
+      <h2 class="header">Empty State</h2>
+      <ul class="record-list">
+        <li class="record">text1</li>
+        <li class="record">text2</li>
+        <li class="record">text3</li>
+      </ul>
+    </div>
+  </div>
+
+</template>
+
+
+<style scoped>
+body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  margin: 0;
+  font-family: Arial, sans-serif;
+}
+
+.container {
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  padding: 25px;
+  width: 600px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.card {
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  padding: 20px;
+  max-width: 600px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+.header {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+.record-list {
+  list-style: none;
+  padding: 0;
+}
+
+.record {
+  background-color: #f9f9f9;
+  padding: 10px;
+  margin-bottom: 10px;
+  border-radius: 4px;
+}
+
+.record:last-child {
+  margin-bottom: 0;
+}
+</style>
