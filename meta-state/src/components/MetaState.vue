a8977a69a8b9ffdf32670d0e359fd52ce738d346
metabynull <bynull@meta-secret.org>
2023-11-11 02:40:52 -0800

    Meta State: build the state from json data

--- meta-state/src/components/MetaState.vue
+++ meta-state/src/components/MetaState.vue
@@ -1,34 +1,97 @@
 <script lang="ts">
-import {defineComponent} from 'vue';
 
-export default defineComponent({
-  name: 'HelloWorld',
-  props: {
-    msg: String,
+export default {
+  name: 'MetaState',
+
+  data() {
+    return {
+      message: 'Here wil be json data from meta-state',
+      jsonData: [
+        {
+          "description": "Client creates a vault",
+          "nodes": [
+            {
+              "name": "device_a",
+              "records": [
+                {
+                  "key": "DeviceA/VaultAudit/test_vault@1",
+                  "value": "JoinRequest { device_a }"
+                }
+              ]
+            },
+            {
+              "name": "device_b",
+              "records": []
+            },
+            {
+              "name": "server",
+              "records": []
+            }
+          ]
+        },
+        {
+          "description": "Server: gets a vault creation request",
+          "nodes": [
+            {
+              "name": "device_a",
+              "records": [
+                {
+                  "key": "DeviceA/VaultAudit/test_vault@1",
+                  "value": "JoinRequest { device_a }"
+                }
+              ]
+            },
+            {
+              "name": "device_b",
+              "records": []
+            },
+            {
+              "name": "server",
+              "records": [
+                {
+                  "key": "DeviceA/VaultAudit/test_vault@1",
+                  "value": "JoinRequest { device_a }"
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
   },
-});
+  methods: {}
+
+}
 </script>
 
 <template>
   <div class="container">
-    <div class="card">
-      <h2 class="header">Empty State</h2>
-      <ul class="record-list">
-        <li class="record">text1</li>
-        <li class="record">text2</li>
-        <li class="record">text3</li>
-      </ul>
+    <div style="display: flex; padding: 10px">
+      <textarea v-model="message"></textarea>
     </div>
 
-    <div style="padding: 10px"></div>
+    <div class="card" style="margin: 10px" v-for="state of jsonData">
+      <h2 class="header">{{ state.description }}</h2>
+      <div v-for="node of state.nodes">
+        <div style="display:flex; justify-content: flex-start;">
+          <em style="text-align: left;">{{ node.name }} records:</em>
+        </div>
 
-    <div class="card">
-      <h2 class="header">Empty State</h2>
-      <ul class="record-list">
-        <li class="record">text1</li>
-        <li class="record">text2</li>
-        <li class="record">text3</li>
-      </ul>
+        <div v-if="node.records.length === 0" style="text-align: left; padding: 10px 10px 20px;">
+          <span>No records</span>
+        </div>
+
+        <div>
+          <ul class="record-list" v-for="record of node.records">
+            <li class="record">
+              <div class="key-value-pair">
+                <span class="key">{{ record.key }}:</span>
+                <span class="value">{{ record.value }}</span>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
 
@@ -46,11 +109,8 @@ body {
 }
 
 .container {
-  border: 1px solid #ddd;
-  border-radius: 4px;
+  display: flex;
   padding: 25px;
-  width: 600px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 }
 
 .card {
@@ -69,6 +129,7 @@ body {
 .record-list {
   list-style: none;
   padding: 0;
+  text-align: justify;
 }
 
 .record {
@@ -80,5 +141,18 @@ body {
 
 .record:last-child {
   margin-bottom: 0;
+}
+
+.key-value-pair {
+  margin-bottom: 10px;
+}
+
+.key {
+  font-weight: bold;
+  margin-right: 5px;
+}
+
+.value {
+  color: #333;
 }
 </style>
