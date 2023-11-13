b7b43a82514e385a52ac35c33c2041acc0ed6579
metabynull <bynull@meta-secret.org>
2023-11-12 23:31:18 -0800

    Meta State: improve visualization and add objects

--- meta-state/src/components/MetaState.vue
+++ meta-state/src/components/MetaState.vue
@@ -16,36 +16,53 @@ export default {
 </script>
 
 <template>
-  <div class="container">
-    <div style="display: flex; padding: 10px">
-      <textarea v-model="message"></textarea>
+  <div style="display: flex; flex-direction: column; text-align: justify" v-for="workflow of jsonData">
+    <div style="padding-left: 35px">
+      <h2> {{ workflow.workflowName }}</h2>
+    </div>
+    <div style="padding-left: 35px">
+      <span> {{ workflow.description }}</span>
     </div>
 
-    <div class="card" style="margin: 10px" v-for="state of jsonData">
-      <h2 class="header">{{ state.description }}</h2>
-      <div v-for="node of state.nodes">
-        <div style="display:flex; justify-content: flex-start;">
-          <em style="text-align: left;">{{ node.name }} records:</em>
+    <div class="container">
+      <div class="card" style="margin: 10px" v-for="state of workflow.states">
+        <div class="header">
+          <span class="text-lg bold">{{ state.description }}</span>
         </div>
 
-        <div v-if="node.records.length === 0" style="text-align: left; padding: 10px 10px 20px;">
-          <span>No records</span>
-        </div>
+        <div v-for="node of state.nodes">
+          <div style="display:flex; justify-content: flex-start;">
+            <span class="text-xl bold" style="text-align: left; padding-right: 5px">{{ node.name }}</span>
+            <span class="text-xl">objects: </span>
+          </div>
 
-        <div>
-          <ul class="record-list" v-for="record of node.records">
-            <li class="record">
-              <div class="key-value-pair">
-                <span class="key">{{ record.key }}:</span>
-                <span class="value">{{ record.value }}</span>
-              </div>
-            </li>
-          </ul>
+          <div v-if="node.objects.length === 0" style="text-align: left; padding: 10px 10px 20px;">
+            <span>No objects</span>
+          </div>
+
+          <div v-for="persistentObject of node.objects">
+            <div style="display:flex; justify-content: flex-start; padding-left: 10px; padding-top: 15px">
+              <span style="text-align: left;">{{ persistentObject.name }} {</span>
+            </div>
+            <ul class="record-list" v-for="record of persistentObject.records">
+              <li class="record">
+                <div style="padding-left: 10px">
+                  <span class="key">{{ record.key }}:</span>
+                  <span class="value">{{ record.value }}</span>
+                </div>
+              </li>
+            </ul>
+
+            <div style="display:flex; justify-content: flex-start; padding-left: 10px; padding-bottom: 15px">
+              <span style="text-align: left;">}</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
 
+    <div style="padding: 5px"></div>
+  </div>
 </template>
 
 
@@ -68,7 +85,6 @@ body {
   border: 1px solid #ddd;
   border-radius: 8px;
   padding: 20px;
-  max-width: 600px;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 }
 
@@ -88,14 +104,11 @@ body {
   padding: 10px;
   margin-bottom: 10px;
   border-radius: 4px;
+  margin-left: 20px;
 }
 
 .record:last-child {
   margin-bottom: 0;
-}
-
-.key-value-pair {
-  margin-bottom: 10px;
 }
 
 .key {
@@ -106,4 +119,19 @@ body {
 .value {
   color: #333;
 }
+
+.text-lg {
+  font-size: 1.125rem;
+  line-height: 1.75rem;
+}
+
+.text-xl {
+  font-size: 1.25rem;
+  line-height: 1.75rem;
+}
+
+.bold {
+  font-weight: 700;
+}
+
 </style>
